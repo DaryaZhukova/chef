38e78bb328bf6573821df4bc59638e86b6056006
Darya Zhukova <darya_zhukova@epam.com>
2018-09-09 21:29:45 +0300

    hometask2

--- cookbooks/task2_nginx/recipes/default.rb
+++ cookbooks/task2_nginx/recipes/default.rb
@@ -7,12 +7,21 @@ service 'nginx' do
   supports :restart => true
   action [ :enable, :start ]
 end 
+
 template '/etc/nginx/conf.d/server.conf' do
   source 'server.erb'
   variables(
-    nodejboss: search(:node, 'roles:jboss')[0]["network"]["interfaces"]["enp0s8"]["addresses"].detect{|k,v| v[:family] == "inet" }.first,
-    jbossip: node["network"]["interfaces"]["enp0s8"]["addresses"].detect{|k,v| v[:family] == "inet" }.first,
-    jbossport: "8080",
-    jbossapp: "sample")
+    jbossip: search(:node, 'roles:jboss')[0]["network"]["interfaces"]["enp0s8"]["addresses"].detect{|k,v| v[:family] == "inet" }.first,
+    jbossport: node['jbossport'],
+    jbossapp: node['jbossapp'],
+    nginxport: node['nginxport'])
+  notifies :restart, 'service[nginx]'
+end
+template '/usr/share/nginx/html/secret.html' do
+  source 'secret.erb'
+  variables(
+    secret: search(:nginx_conf, "id:file"))
+  sensitive true
+  notifies :restart, 'service[nginx]', :immediately
 end
 
